39b76bbdfb3a482e59e18427f5078d3f67d6f1f5
Alexander Carpio Mamani <62957804+dretcm@users.noreply.github.com>
2021-01-08 22:16:40 -0500

    Update TUTORIAL_PYGAME.py

--- TUTORIAL_PYGAME.py
+++ TUTORIAL_PYGAME.py
@@ -6,12 +6,14 @@ from pygame.locals import *  # for QUIT, etc
 clock = pygame.time.Clock()  # for the 'x' fps
 WINDOW_SIZE = (400, 400)
 
-pygame.display.set_caption('My Pygame window ')   # set the name of window
-screen = pygame.display.set_mode(WINDOW_SIZE, 0, 32)  # set the size of window
+pygame.init() # initial pygame
+
+pygame.display.set_caption('My Pygame window ')   # set window´s name
+screen = pygame.display.set_mode(WINDOW_SIZE, 0, 32)  # set the size of window, or initial the screen
 
 while True:
         
-        for event in pygame.event.get():  # events at interact with the display, window.
+        for event in pygame.event.get():  # events at interact with the display, window, screen.
                 if event.type == QUIT:  # QUIT is event type or message of exit or close pygame´s window.
                         pygame.quit() # exit from pygame
                         sys.exit()  # exit from Python
