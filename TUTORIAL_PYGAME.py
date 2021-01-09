767af7ba8ab0ea4e7b2128ed748bb33afeb59310
Alexander Carpio Mamani <62957804+dretcm@users.noreply.github.com>
2021-01-08 22:00:33 -0500

    Create TUTORIAL_PYGAME.py

---
+++ TUTORIAL_PYGAME.py
@@ -0,0 +1,22 @@
+# (1) ------------------------------------ window, rename, size, quit ---------------------------------- #
+
+import pygame, sys
+from pygame.locals import *  # for QUIT, etc
+
+clock = pygame.time.Clock()  # for the 'x' fps
+WINDOW_SIZE = (400, 400)
+
+pygame.display.set_caption('My Pygame window ')   # set the name of window
+screen = pygame.display.set_mode(WINDOW_SIZE, 0, 32)  # set the size of window
+
+while True:
+        
+        for event in pygame.event.get():  # events at interact with the display, window.
+                if event.type == QUIT:  # QUIT is event type or message of exit to pygame
+                        pygame.quit() # exit from pygame
+                        sys.exit()  # exit from Python
+                        
+        pygame.display.update()
+        clock.tick(60)  # for the 60 fps
+
+# (2) ------------------------------------ window, rename, size, quit ---------------------------------- #
