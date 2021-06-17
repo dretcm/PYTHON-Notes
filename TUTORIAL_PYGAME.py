d36162689d7afdaa887047a5692aa7eac46ff0e6
dretcm <62957804+dretcm@users.noreply.github.com>
2021-06-16 21:53:18 -0500

    update_of_16_june

--- TUTORIAL_PYGAME.py
+++ TUTORIAL_PYGAME.py
@@ -166,6 +166,13 @@ pygame.mixer.music.queue("test2.mp3")
 pygame.mixer.music.play(loops=-1, start=35)
 
 
+# stop:
+pygame.mixer.music.stop()  # finish the music.
+
+# if put the next line od code, it will continue since the begin.
+pygame.mixer.music.play()
+
+
 # music and difference with sounds : mixer Sound :
 
 https://programmerclick.com/article/704172167/
@@ -188,6 +195,7 @@ while True:
                 if event.type == pygame.KEYDOWN:
                         if event.key == pygame.K_SPACE:
                                 sound.play()  # play the sound object.
+				# sound.stop()
                                 
         pygame.display.update()
 
@@ -213,6 +221,7 @@ bg = pygame.transform.scale(pygame.image.load('bg.png'), (width, height)) # (ima
 score = 0
 apple_img = pygame.transform.scale(pygame.image.load('apple.png'), (25, 25))
 apple_img.set_colorkey((255, 255, 255))  # color which will be transparent o equals to background
+# https://www.remove.bg/upload for remove bg
 
 apple = pygame.Rect(0, 0, 25, 25)
 state_apple = True
@@ -310,6 +319,61 @@ while True:
                         
         pygame.display.update()
         clock.tick(30)
+
+
+# button avance with yes and no :
+
+        def push_button(self, text ='nothing'):
+
+                font = pygame.font.Font(None,70)
+                on = False
+
+                midle_x, midle_y = list(map(lambda x: x//2, self.WINDOW_SIZE))
+
+                pos_yes = (midle_x-110, midle_y+50)
+                yes = pygame.Rect(pos_yes[0], pos_yes[1], 100,70)
+                message_yes = font.render('Yes', 1, (0,0,0))
+
+                pos_no = (midle_x+20, midle_y+50)
+                no = pygame.Rect(pos_no[0], pos_no[1], 100,70)
+                message_no = font.render('No', 1, (0,0,0))
+
+                pos_msg = (midle_x-150,midle_y-40)
+                message = font.render(text,1,(0,0,0))
+
+                self.screen.fill((0,0,0))
+
+                while True:
+                        for event in pygame.event.get():
+                                if event.type == QUIT:
+                                        self.exit_game()
+                                if event.type == pygame.MOUSEBUTTONDOWN:
+                                        if event.button == 1 and yes.collidepoint(event.pos):
+                                                print('you clicked the button yes')
+                                                on = True
+                                                return on
+                                                
+                                if event.type == pygame.MOUSEBUTTONDOWN:
+                                        if event.button == 1 and no.collidepoint(event.pos):
+                                                print('you clicked the button no')
+                                                return on
+                                                
+                        pygame.draw.rect(self.screen, (255,100,0), yes)
+                        self.screen.blit(message_yes, pos_yes)
+                        
+                        pygame.draw.rect(self.screen, (255,100,0), no)
+                        self.screen.blit(message_no, pos_no)
+                        
+                        pygame.draw.rect(self.screen, (255,255,255), [pos_msg[0], pos_msg[1],300, 60])
+                        self.screen.blit(message, pos_msg)
+                        
+                        pygame.display.update()
+                        self.clock.tick(30)
+                        
+        def exit_game(self):
+                pygame.quit()
+                sys.exit()
+
                         
 # (7) ------------------------------------ surface, center of image ---------------------------------- #
 
@@ -351,7 +415,6 @@ while True:
         pygame.display.update()  # update all frames and goo
         clock.tick(30)
 
-# (8) ------------------------------------ dialog player, camera ---------------------------------- #
 # (9) ------------------------------------ input text  ---------------------------------- #
 
 # example 1:
@@ -1041,5 +1104,223 @@ while True:
         pygame.display.update()
         clock.tick(30)
 
+
+
+# option 2 :
+
+import math, pygame, random
+
+pygame.init()
+
+class Direccion:  # Padre
+        resultante_x = 0
+        resultante_y = 0
+        
+        x = 0
+        y = 0
+        
+        h = 0
+        k = 0
+        
+        theta = 0
+        x_cos = 0
+        y_sin = 0
+        
+        magnitud = 0
+
+        collider = pygame.Rect(0,0,0,0)
+        
+        def mover_collider(self):
+                self.collider.x = (self.magnitud * self.x_cos) + self.h
+                self.collider.y = (self.magnitud * self.y_sin) + self.k
+                
+        def vector_respecto_al_origen(self):
+                # (resultante_x, resultante_y) = (x - h, y - k)
+                self.resultante_x = self.x - self.h
+                self.resultante_y = self.y - self.k
+        
+        def calcular_angulo(self):
+                # [0 - 180]   : +
+                # (180 - 360] : -
+                
+                # atan2: [-pi,pi] # atan: [-pi/2, pi/2]
+                self.theta =  math.degrees(math.atan2(self.resultante_y, self.resultante_x))
+                
+                if self.theta < 0:
+                        self.theta += 360.0
+                                
+                self.x_cos = math.cos(math.radians(self.theta))
+                self.y_sin = math.sin(math.radians(self.theta))
+
+        def get_collider(self):
+                return self.collider
+        
+        def get_position(self):
+                return (self.collider.x, self.collider.y)
+        
+
+class Bullet(Direccion):  # Hijo(Padre) : hereda los atributos y metodos del padre.
+        speed = 20
+        limit = 700
+
+        radius = 10
+        
+        def __init__(self):
+                self.collider= pygame.Rect(0,0,self.radius,self.radius)
+        
+        def shoot(self, screen):
+                self.mover_collider()
+
+                pygame.draw.circle(screen, (255,255,255), self.get_position(), self.radius)
+
+                if self.magnitud > self.limit:
+                        return False
+                else:
+                        self.magnitud += self.speed
+                        return True
+                
+        def init_shoot(self, vector_position, player_position):
+                self.h, self.k = player_position
+                self.x, self.y = vector_position
+                
+                self.vector_respecto_al_origen()
+                self.calcular_angulo()
+
+
+class Gun:
+        bullets = []
+        bullets_activate = []
+
+        store = 10
+        limit_store = 30
+        ammunition = 10
+
+        def update_gun(self, screen):
+                if self.bullets:
+                        for bullet in self.bullets:
+                                if bullet.shoot(screen):
+                                        self.bullets_activate.append(bullet)
+                                        
+                        self.bullets = self.bullets_activate.copy() # usamos ".copy()" para evitar la mismidad
+                        self.bullets_activate = []
+
+        def activate_gun(self, vector_position, player_position):
+                if self.store > 0:
+                        bullet = Bullet()
+                        bullet.init_shoot(vector_position, player_position)
+                        
+                        self.bullets.append(bullet)
+
+                        self.store -= 1
+
+##        def reload_ammunition(self):
+##                for i in range(self.ammunition):
+##                        if self.limit_store > self.store:
+##                                self.store += 1
+##                        else:
+##                                break
+                        
+        def reload_ammunition(self):  # recursividad
+                if self.ammunition == 0:
+                        self.ammunition = 10
+                else:
+                        if self.store < self.limit_store:
+                                self.store += 1
+                                self.ammunition -= 1
+                                self.reload_ammunition()
+
+                        
+        def get_ammunition(self):
+                return self.store
+
+        def get_colliders(self):
+                return self.bullets
+        
+        
+class Enemy(Direccion):
+        range_speed = [2, 5, 10, 20]
+        width = 100
+        height = 80
+        
+        def __init__(self, size_screen):
+                begin = -10
+                
+                if random.randint(0,1):
+                        self.h = random.choice([begin, size_screen[0]])
+                        self.k = random.randint(begin, size_screen[1])
+                else:
+                        self.h = random.randint(begin, size_screen[0])
+                        self.k = random.choice([begin, size_screen[1]])
+
+                self.collider = pygame.Rect(self.h, self.k, self.width, self.height)
+                
+                img = random.choice(['images/otaku_1.png', 'images/otaku_2.png','images/viejo.png','images/rosel.png'])
+                self.enemy_image = pygame.transform.scale(pygame.image.load(img), (self.width, self.height))
+                
+                self.magnitud = random.choice(self.range_speed)
+        
+        def follow_player(self, screen, player_position):
+                self.x, self.y = player_position
+                self.h, self.k = self.get_position()
+                
+                self.vector_respecto_al_origen()
+                self.calcular_angulo()
+
+                self.mover_collider()
+
+                screen.blit(self.enemy_image, self.get_position())
+
+
+class Orda:
+        enemys = []
+        enemys_activate = []
+        
+        t = 0
+        limit_t = 50
+
+        score = 0
+        font = pygame.font.Font(None,50)
+        
+        def __init__(self, size_screen):
+                self.size_screen = size_screen
+
+        def update_enemys(self, screen, player, pistol):
+                for enemy in self.enemys:
+                        enemy.follow_player(screen, player.get_position())
+
+                        state = True
+                        for bullet in pistol.get_colliders():
+                                if enemy.get_collider().colliderect(bullet.get_collider()):
+                                        state = False
+                                        #enemy.magnitud = -5  # if comment state = False and activate this line, when the enemys colliderect with the bullet it back off.
+                        if state:
+                               self.enemys_activate.append(enemy)
+                        else:
+                                self.score += 1
+                               
+                        if enemy.get_collider().colliderect(player.get_collider()):
+                                print('end game')
+                                player.set_player_live(False)
+                                
+                self.enemys = self.enemys_activate.copy()  # usamos ".copy()" para evitar la mismidad
+                self.enemys_activate = []
+
+                if self.t < self.limit_t:
+                        self.t += 1
+                else:
+                        self.activate_enemy()
+                        self.t = 0
+
+                self.show_score(screen)
+
+        def activate_enemy(self):
+                enemy = Enemy(self.size_screen)
+                self.enemys.append(enemy)
+
+        def show_score(self, screen):
+                message = self.font.render('score: '+ str(self.score), 1, (255,255, 255))
+                screen.blit(message, (self.size_screen[0]-170,10))
+
+    
 # (16) ------------------------------------ input text  ---------------------------------- #
 # (17) ------------------------------------ input text  ---------------------------------- #
